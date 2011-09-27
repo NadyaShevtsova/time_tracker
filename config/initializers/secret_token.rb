d9e0e9fe3088e4ad03379ffb80bccd274041a3ee
Nadya Shevtsova <nadya.shevtsova@gmail.com>
2011-09-27 20:21:54 +0300

    Created new project time_tracker

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimeTracker::Application.config.secret_token = '0ba0a4f9f40452ba81ff733231c6b7ba7ed6eb36dc5160f8550edd51f81e52986f25ba076f19f396d16d3f00e5f27c5d28e870a7dc859ae0cdc8b7dd1c94e00c'
