d14dac93bcd400c67d619490219d29fbbdc54752
Nadya Shevtsova <nadya.shevtsova@gmail.com>
2011-09-28 19:28:25 +0300

    added validation to model user.rb for field username,inset flash notices to layouts

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :username, :password, :password_confirmation, :remember_me
+
+  validates :username, :presence => true
+
 end
