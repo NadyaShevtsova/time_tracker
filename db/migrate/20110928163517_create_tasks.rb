5359868e68bc02df0d2e7b6a0c4b071916418d3d
Nadya Shevtsova <nadya.shevtsova@gmail.com>
2011-09-28 19:47:30 +0300

    filled migration tasks

--- db/migrate/20110928163517_create_tasks.rb
+++ db/migrate/20110928163517_create_tasks.rb
@@ -1,6 +1,10 @@
 class CreateTasks < ActiveRecord::Migration
   def self.up
     create_table :tasks do |t|
+      t.string   :description
+      t.datetime :start_time
+      t.datetime :end_time
+      t.string   :external_link
 
       t.timestamps
     end
