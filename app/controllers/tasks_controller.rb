8dfe9c5dbbff5c33710b0fa0218d2b6721ca1d5f
Nadya Shevtsova <nadya.shevtsova@gmail.com>
2011-09-28 21:53:40 +0300

    added rout resorces :tasks to routs.rb,created new projec by mins of console

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,4 +5,18 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params[:task])
+
+    if @task.save
+      format.html { redirect_to(@task, :notice => 'Task was succesfully created')}
+    else
+      format.html { render :action => "new"}
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -60,5 +60,7 @@ TimeTracker::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
 
+  resources :tasks
+
   root :to => 'tasks#index'
 end
