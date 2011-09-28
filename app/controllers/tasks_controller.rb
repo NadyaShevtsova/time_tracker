5b9397695ad065b5c40c8d1ae3dc10550d9dbd37
Nadya Shevtsova <nadya.shevtsova@gmail.com>
2011-09-28 23:29:17 +0300

    added methods edit and update to controller task, added view edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,6 +9,10 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
   def show
     @task = Task.find(params[:id]) 
   end
@@ -20,6 +24,16 @@ class TasksController < ApplicationController
        redirect_to(@task, :notice => 'Task was succesfully created')
     else
        render :action => "new"
+    end
+  end
+ 
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update_attributes(params[:task])
+      redirect_to(@task, :notice => 'Task was successfully updated.')
+    else
+      render :action => "edit"
     end
   end
 
