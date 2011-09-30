a100d5ee62e451262fa1a20422e34e879bfba5b2
Nadya Shevtsova <nadya.shevtsova@gmail.com>
2011-09-30 11:14:57 +0300

    modified model/task: added validates_datetime, instaled gem validatestimeliness

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,5 +3,6 @@ class Task < ActiveRecord::Base
 
   validates :description, :start_time, :end_time, :presence => true
   validates_url_format_of :external_link, :allow_nil => true, :allow_blank => true, :message => "incorrect external link"
+  validates_datetime :end_time, :after => :start_time, :after_message => "must be at least Start time"
 
 end
