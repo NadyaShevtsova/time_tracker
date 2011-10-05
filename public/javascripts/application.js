cc99752b343b9ae2694f295fcf005e72dca233c3
Nadya Shevtsova <nadya.shevtsova@gmail.com>
2011-10-05 22:05:25 +0300

    added selected day in calendar, remove method js to view index

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,16 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-$(document).ready(function() {
-  $('#calendar').datepicker({
-    setDate: new Date(2008,9,03),
-    onSelect: function(dataText){
-      window.location.href = "/?date=" + dataText;
-    }
-  });
-
-});
-
 
 function add_project(){
   if ( $('#task_project_id option:selected').text() == "add project" ) {
