71ebf96a456d7ad62408c2b3e51d6b78119d8958
Nadya Shevtsova <nadya.shevtsova@gmail.com>
2011-10-07 11:39:18 +0300

    fixed external link, added seeds

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,7 +3,15 @@ class Task < ActiveRecord::Base
   belongs_to :user
 
   validates :description, :start_time, :end_time,  :user_id, :project_id, :presence => true
-  validates_url_format_of :external_link, :allow_nil => true, :allow_blank => true, :message => "incorrect external link"
   validates_datetime :end_time, :after => :start_time, :after_message => "must be at least Start time"
+  validates :external_link, :format => {:with => /((http|https):\/\/|[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)+.*)$/}
+
+  before_validation :check_http
+
+  private
+
+  def check_http
+     self.external_link = "http://#{self.external_link}" if self.external_link.grep(/(http:\/\/|https:\/\/)/).empty?
+  end
 
 end
