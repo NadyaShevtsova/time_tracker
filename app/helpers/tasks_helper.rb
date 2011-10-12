d82448be8fad696a6c1add0327577c34dcaa667e
Nadya Shevtsova <nadya.shevtsova@gmail.com>
2011-10-12 23:22:32 +0300

    added task helper method show_weeks for report

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,14 @@
 module TasksHelper
+  def show_weeks
+    date_now = (params[:date]).nil? ? Date.today : Date.parse(params[:date])
+    current_day = date_now.beginning_of_month.beginning_of_week
+    links = ""
+    6.times do 
+      links += link_to "week (#{current_day.strftime("%Y/%m/%d")} - #{current_day.end_of_week.strftime("%Y/%m/%d")})", root_path(:start_date => current_day.strftime("%Y/%m/%d"), :end_date => current_day.end_of_week.strftime("%Y/%m/%d"))
+      links += "<br>"
+      current_day = current_day + 7.days
+      break if current_day.month !=  date_now.month
+    end
+    links
+  end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -16,22 +16,6 @@ $(function() {
   });
 });
 
-$(function() {
-  // if the function argument is given to overlay,
-  // it is assumed to be the onBeforeLoad event listener
-  $("a[rel='#overlay']").overlay({
-    mask: '#000',
-    effect: 'apple',
-
-    onBeforeLoad: function() {
-      // grab wrapper element inside content
-      var wrap = this.getOverlay().find(".contentWrap");
-      // load the page specified in the trigger
-      wrap.load(this.getTrigger().attr("href"));
-    }
-  });
-});
-
 
 function add_project(){
   if ( $('#task_project_id option:selected').text() == "add_project" ) {
