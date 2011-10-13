f4a81322c78f245e7520bfac78ff3261a5dcd2f3
Nadya Shevtsova <nadya.shevtsova@gmail.com>
2011-10-13 09:53:38 +0300

    added saving settings for per_page (per_page in session)

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,5 @@
 class ReportsController < ApplicationController
+
   def index
     @list = Task.where(:start_time => DateTime.parse(params[:start_date])..DateTime.parse(params[:end_date])).group("user_id, task_name")
   end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,12 +2,12 @@ class TasksController < ApplicationController
   before_filter :authenticate_user!
   #before_filter :new_project, :only => [:create, :update]
   before_filter :check_day_for_user, :except => [:index, :new, :create]
+  before_filter :transfer_options, :only => [:index]
   helper_method :sort_column, :sort_direction
 
   def index
     @current_date = (params[:date]).nil? ? Date.today : Date.parse(params[:date])
-   
-   @tasks = (current_user.admin ? Task : current_user.tasks).where('start_time >= ? and end_time <= ?', @current_date.beginning_of_day, @current_date.end_of_day).paginate :page => params[:page], :per_page => (params[:per_page]).nil? ? 3 : params[:per_page], :include => ["project","user"], :order => "#{sort_column} #{sort_direction}"
+   @tasks = (current_user.admin ? Task : current_user.tasks).where('start_time >= ? and end_time <= ?', @current_date.beginning_of_day, @current_date.end_of_day).paginate :page => params[:page], :per_page => session[current_user.id][:per_page], :include => ["project","user"], :order => "#{sort_column} #{sort_direction}"
   end
 
   def new
@@ -75,6 +75,14 @@ class TasksController < ApplicationController
   end
 
   private
+  
+  def transfer_options
+    unless params[:per_page].blank?
+      session[current_user.id] = {:per_page => params[:per_page]}
+    else
+      session[current_user.id] ||= {:per_page => 3}
+    end
+  end
 
   def sort_column
     Task.column_names.include?(params[:sort]) ? params[:sort] : params[:sort] == "projects.name" ? params[:sort] : params[:sort] == "users.username" ? params[:sort] :"start_time"
