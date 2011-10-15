eb3564bb0f33bee01a2acd312bf17bcaf8e646df
Vitaliy Shvetsov <vshevtsov@r-ideas.org>
2011-10-15 12:00:21 +0300

    deleted uniquness for task_name from validations in model task.rb

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,13 +4,13 @@ class Task < ActiveRecord::Base
 
   accepts_nested_attributes_for :project
 
-  validates :task_name, :presence => true, :uniqueness => true
+  validates :task_name, :presence => true
   validates :description, :start_time, :end_time,  :user_id, :presence => true
   validates_datetime :end_time, :after => :start_time, :after_message => "must be at least Start time"
   validates :external_link, :format => {:with => /((http|https):\/\/|[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)+.*)$/}, :allow_blank => true
 
   before_validation :check_http
-  validate :check_equality_day 
+  validate :check_equality_day
 
 
   private
