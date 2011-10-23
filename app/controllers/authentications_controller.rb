1bc8585c3c32cdc742c35873841b06e7e807bf4b
Nadya Shevtsova <nadya.shevtsova@gmail.com>
2011-10-23 12:23:32 +0300

    added button twitter and facebook to view edit_user,in contoller autintifications/creat rewrite redirect_to: edi_user

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -3,8 +3,8 @@ class AuthenticationsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"] 
     current_user.authentications.create(:provider => auth ['provider'], :uid => auth['uid'])
-    flash[:notice] = "Authentication successful."
-    redirect_to authentications_url
+    flash[:notice] = "Association with #{auth['provider']} successful."
+    redirect_to edit_user_registration_path
   end
 
 end
