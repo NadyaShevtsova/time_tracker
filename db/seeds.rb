aa6d96e2c1d4c9ef21b9b17cf781998ceb0cc421
Nadya Shevtsova <nadya.shevtsova@gmail.com>
2011-10-24 15:11:18 +0300

    content of file script/load_tasks.rb moved to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,34 @@
 
 User.create(:email => "admin@gmail.com", :password => "admin",:password_confirmation => "admin", :username => "admin", :admin => true)
 
+Project.transaction do
+  (1..7).each do |i|
+    Project.create(:name => Faker::Company.name)
+  end
+end
+
+User.create(:username => "test", :email => "test@test.com", :password => "test", :password_confirmation => "test")
+
+User.create(:email => "user@user.com", :password => "user",:password_confirmation => "user", :username => "user", :admin => false)
+
+user_id1 = User.find_by_username("test").id
+user_id2 = User.find_by_username("user").id
+
+Task.transaction do
+  (-5..5).each do |i|
+    (1..7).each do |j|
+      Task.create(:user_id => user_id1, :project_id => j, :task_name => Faker::Company.name,:description => Faker::Lorem.sentence, :external_link => Faker::Internet.domain_name, :start_time => (Date.today + i.days).strftime("%Y/%m/%d 11:00"), :end_time => (Date.today + i.days).strftime("%Y/%m/%d 14:55"))
+    end
+  end
+
+end
+Task.transaction do
+  (-5..5).each do |i|
+    (1..7).each do |j|
+      Task.create(:user_id => user_id2, :project_id => j, :task_name => Faker::Company.name,:description => Faker::Lorem.sentence, :external_link => Faker::Internet.domain_name, :start_time => (Date.today + i.days).strftime("%Y/%m/%d 11:00"), :end_time => (Date.today + i.days).strftime("%Y/%m/%d 14:55"))
+    end
+  end
+
+end
+
+
