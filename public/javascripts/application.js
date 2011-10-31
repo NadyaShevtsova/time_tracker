586f097fb0d1bee475506af35d560263a71d4b57
Nadya Shevtsova <nadya.shevtsova@gmail.com>
2011-10-31 20:10:22 +0200

    started google maps

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -30,8 +30,45 @@ function add_project(){
 function per_page(value, direction, sort) {
   url = window.location.search.replace(/(&|\\?).per_page=\d*/g, '').replace(/\\?page=\d*/g, '').replace(/(&|\\?).direction=(asc|desc)/, '').replace(/(&|\\?).sort=\w*(\.||_)\w*/, '');
   window.location = url + ((value) ? ((url == '' ? '?':'&') + 'per_page=' +  value):'') + ((direction) ? ("&direction=" + direction) : '') + ((sort) ? ("&sort=" + sort) : '');
-  
-
 }
+
+
+$(document).ready(function() {
+  // объявление переменных
+var map 		= null; // карта
+var geocoder 	= null; // объект геокодинга
+
+// инициализация карты
+function initialize() {
+	if (GBrowserIsCompatible()) {
+		map = new GMap2(document.getElementById("map_canvas")); // делаем карту в таком диве
+		map.setCenter(new GLatLng(59.94, 30.3), 13); // устанавливаем центр карты и приближение
+		geocoder = new GClientGeocoder(); // создаем объект геокодера
+	}
+}
+
+// функция, выводящая маркер, 
+// соответсвующий указанному адресу
+// ее аргумент — адрес места
+function showAddress(address) {
+	if (geocoder) {
+		geocoder.getLatLng(
+			address, // сообщаем объекту адрес
+			function(point) { // объект вызывает функцию, аргумент которой точка, соответствующая адресу
+				if (!point) { // если точка не определена (то есть адрес не правильный или Гугл не нашел его)
+					alert(address + " не найден"); // выводим ошибку
+				} else { // если же все круто, то
+					map.setCenter(point, 15); // центром карты делаем эту точку
+					var marker = new GMarker(point); // создаем в ней маркер
+					map.addOverlay(marker); // и добавляем его на карту
+					marker.openInfoWindowHtml(address); // открываем у него инфоокно
+					document.getElementById('spot-lat').value 	= point.y; // и добавляем в поля широту
+					document.getElementById('spot-longi').value = point.x; // и долготу
+				}
+			}
+		);
+	}
+}
+});
 
 
