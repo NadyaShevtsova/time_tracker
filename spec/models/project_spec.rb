214649b84707297fab7c149b557e5b4a04942da7
Nadya Shevtsova <nadya.shevtsova@gmail.com>
2011-11-09 15:30:45 +0200

    expanded spec/model/project_spec.rb it'should not be valid with the same names'

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,16 +1,20 @@
 require File.dirname(__FILE__) + '/../spec_helper'
  
   describe Project do
-    it "Created project should be valid" do
-      project = Factory.create(:project)
+    it "should not be valid with the same names" do
+      project = Factory.create(:project, :name => "Schaefer-Haley")
       project.errors.should be_blank
       project.should be_valid
+
+      project2 = Factory.build(:project, :name => "Schaefer-Haley")
+      project2.should_not be_valid
+      project2.errors[:name].should_not be_blank
     end
 
-    it "Project with blank name should not be created" do
-      project = Factory.build(:project, :name => '')
+    it "should not be created with blank name" do
+      project = Factory.build(:project, :name => nil)
       project.should_not be_valid
       project.errors[:name].should_not be_blank
     end
-
+    
   end
