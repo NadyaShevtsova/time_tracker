4d419e6326eac2bd71d1c4a69525111b8998ce9b
Nadya Shevtsova <nadya.shevtsova@gmail.com>
2011-11-10 09:23:40 +0200

    added spec test for method :check_http

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,23 +1,48 @@
 require File.dirname(__FILE__) + '/../spec_helper'
  
   describe Task do
-    it "Created task should be valid" do
+    it "should be valid" do
       task = Factory.create(:task)
-      task.errors.should be_blank
+      task.should_not be_new_record
       task.should be_valid
     end
     
     it "Task with blank task_name, description should not be created" do
-      task = Factory.build(:task, :task_name => '', :description => '')
+      task = Factory.build(:task, :task_name => nil, :description => nil,:start_time => '',:end_time => '')
       task.should_not be_valid
       task.errors[:task_name].should_not be_blank
       task.errors[:description].should_not be_blank
+      task.errors[:start_time].should_not be_blank
+      task.errors[:end_time].should_not be_blank
     end
     
     it "should not be created if day of :start_time not equal day of :end_time" do
       task = Factory.build(:task, :start_time => '2011-11-01 09:00:00', :end_time => '2011-11-02 12:55:00')
       task.should_not be_valid
       (task.send :check_equality_day).should_not be_blank
+    end
+
+    it "should be created with empty :external_link" do
+      task = Factory.create(:task, :external_link => '')
+      task.send :check_http
+      task.should be_valid
+      task.external_link.should be_blank
+    end
+
+    it "should be successful created with :external_link => 'mail.ru' " do
+      task = Factory.create(:task, :external_link => 'mail.ru')
+      task.send :check_http
+      task.should be_valid
+      task.external_link.grep(/(http:\/\/|https:\/\/)/).empty? == false
+      task.external_link == 'http://mail.ru'
+    end
+
+    it "should be successful created with :external_link => 'http://mail.ru' " do
+      task = Factory.create(:task, :external_link => 'http://mail.ru')
+      task.external_link.grep(/(http:\/\/|https:\/\/)/).empty? == false
+      task.send :check_http
+      task.should be_valid
+      task.external_link == 'http://mail.ru'
     end
 
 
